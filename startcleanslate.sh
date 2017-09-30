466e7c0697d66371d2d18812d1259c333df3260b
paolo <pierpaolo@bettoni.org>
2017-09-29 23:28:34 -0600

    fixed start script

--- startcleanslate.sh
+++ startcleanslate.sh
@@ -1,7 +1,7 @@
 # Delete all containers
-docker rm $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
 # Delete all images
-docker rmi $(docker images -q)
+#docker rmi $(docker images -q)
 # build
 docker-compose build
 # run
